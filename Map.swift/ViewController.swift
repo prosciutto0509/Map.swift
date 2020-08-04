a9dff49108bd8818c0d464540e0c6132805399fb
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-08-04 23:07:19 +0900

    ファーストコミット

--- Map.swift/ViewController.swift
+++ Map.swift/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import MapKit
+class ViewController: UIViewController, UITextFieldDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        inputText.delegate = self
     }
 
-
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var dispMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        
+        textField.resignFirstResponder()
+        
+        if let searchKey = textField.text{
+            
+            print(searchKey)
+            
+            let geocoder = CLGeocoder()
+            
+            geocoder.geocodeAddressString(searchKey , completionHandler: { (placemarks,
+                                                                             error) in
+                
+                if let unwrapPlacemarks = placemarks {
+                    
+                    if let firstPlacemark = unwrapPlacemarks.first {
+                        
+                        if let location = firstPlacemark.location {
+                            
+                            let targetCoordinate = location.coordinate
+                            
+                            print(targetCoordinate)
+                            
+                            let pin = MKPointAnnotation()
+                            
+                            pin.coordinate = targetCoordinate
+                            
+                            pin.title = searchKey
+                            
+                            self.dispMap.addAnnotation(pin)
+                            
+                            self.dispMap.region = MKCoordinateRegion(center: targetCoordinate,  latitudinalMeters:
+                                500.0, longitudinalMeters: 500.0)
+                        }
+                    }
+                }
+                
+            })
+        }
+        return true
+    }
 }
 
